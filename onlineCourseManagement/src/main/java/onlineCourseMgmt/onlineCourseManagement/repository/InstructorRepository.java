e5b1ea5c61717ed354eeaee1cd5adc7c53141d20
someswari24 <someswari.karanam@atyeti.com>
2025-07-10 15:48:19 +0530

    Delete onlineCourseManagement directory

--- onlineCourseManagement/src/main/java/onlineCourseMgmt/onlineCourseManagement/repository/InstructorRepository.java
+++
@@ -1,6 +0,0 @@
-package onlineCourseMgmt.onlineCourseManagement.repository;
-
-import onlineCourseMgmt.onlineCourseManagement.model.Instructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructorRepository extends JpaRepository<Instructor, Long> {}
--- onlineCourseManagement/src/main/java/onlineCourseMgmt/onlineCourseManagement/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package onlineCourseMgmt.onlineCourseManagement.repository;
-
-import onlineCourseMgmt.onlineCourseManagement.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {}
-
