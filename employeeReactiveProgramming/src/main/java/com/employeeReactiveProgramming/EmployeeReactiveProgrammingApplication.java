8e64a89e0badbaa1862a18ca23d7af06584528d4
someswari24 <someswari.karanam@atyeti.com>
2025-09-17 12:41:56 +0530

    Explored Reactive Programming

---
+++ employeeReactiveProgramming/src/main/java/com/employeeReactiveProgramming/EmployeeReactiveProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.employeeReactiveProgramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeReactiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeReactiveProgrammingApplication.class, args);
+	}
+
+}
